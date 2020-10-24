59bdb0ac56cbfa2183b3d332c7818303c342f837
Pål Grønås Drange <pgdr@equinor.com>
2020-10-24 14:30:50 +0200

    Add description and license to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 setuptools.setup(
     name="benchmcmc",
     packages=["benchmcmc"],
+    description="Use MCMC to do benchmark analysis",
+    license="MIT",
     version="0.0.1",
     install_requires=["matplotlib", "pymc3"],
     entry_points={"console_scripts": ["benchmcmc=benchmcmc:main"]},
