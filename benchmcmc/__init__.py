220833c455b34a02ccd68ffa917f8d078b665743
Pål Grønås Drange <pgdr@equinor.com>
2020-10-24 13:11:18 +0200

    Make project into package

---
+++ benchmcmc/__init__.py
@@ -0,0 +1,6 @@
+from .benchmark import run_benchmark
+from .mkbench import main as benchmk
+
+
+def main():
+    pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="benchmcmc",
+    packages=["benchmcmc"],
+    version="0.0.0",
+    install_requires=["matplotlib", "pymc3"],
+    entry_points={
+        "console_scripts": ["benchmcmc=benchmcmc:main", "benchmk=benchmcmc:benchmk"]
+    },
+)
